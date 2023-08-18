a9009e3715fa15e00d2282795bfe2d4ed306b045
johana zapata huertas <johana.9519@hotmail.com>
2023-08-18 12:45:38 -0300

    segunda prenterga

--- .js/main.js
+++ .js/main.js
@@ -1,8 +1,133 @@
 
+//SEGUNDA ENTREGA
+
+const usuario = "maria";
+const contraseña = 123456;
+
+let contraseñaIngresada = prompt("Bienvenida Maria\nIngresa contraseña");
+
+if (parseInt(contraseñaIngresada) === contraseña) {
+  alert("Ingreso exitoso");
+} else {
+
+  let intentos = 1;
+  for (; intentos <= 3; intentos++) {
+    contraseñaIngresada = prompt("La contraseña es incorrecta. Se bloqueará en el tercer intento");
+    if (parseInt(contraseñaIngresada) === contraseña) {
+      alert("Contraseña válida");
+      break;
+    }
+  }
+
+  if (intentos > 3) {
+    alert("Intentos fallidos\nBloqueamos tu cuenta por seguridad");
+  }
+}
+
+const producto1 = {
+  nombre: "rodillo de jade",
+  unidades: 2,
+  material: "cuarzo",
+  precio: 1500,
+  colores: ["rosa", "magenta", "violeta"]
+};
+
+const producto2 = {
+  nombre: "masajedora electrico",
+  unidades: 1,
+  material: "cuarzo",
+  precio: 1500,
+  colores: ["magenta", "verde"]
+};
+
+const producto3 = {
+  nombre: "Gua sha",
+  unidades: 1,
+  material: "cuarzo",
+  precio: 999,
+  colores: ["amarillo", "verde", "magenta"]
+};
+
+const producto4 = {
+  nombre: "bolsas de masaje",
+  unidades: 8,
+  material: "silicona",
+  precio: 1700,
+  colores: ["amarillo", "magenta", "verde"]
+};
+
+const miArray = [producto1, producto2, producto3, producto4];
+
+const productosCaros = miArray.filter(producto => producto.precio > 1300);
+console.log("Mostrar los productos superiores a 1300:", productosCaros);
+
+for (let i = 0; i < miArray.length; i++) {
+  console.log("Mostrar", miArray[i]);
+}
+ 
+
+function nombreDeProductosBuscados() {
+  const productosBuscados = prompt("Ingrese los productos a buscar, separados por comas");
+  return productosBuscados.split(',').map(producto => producto.trim());
+}
+
+const productosABuscar = nombreDeProductosBuscados();
+
+for (const producto of productosABuscar) {
+  for (const item of miArray) {
+    if (item.nombre === producto) {
+      console.log(`Nombre de producto buscado: ${producto}, Valor: ${item.unidades}`);
+      break;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////// Primera   entrega ////////////////////////
 // es un contador de prendas en stock 
 
 
-let contraseña = "123456"
+/*let contraseña = "123456"
 
 let intentos = 3
 let contador = 1
